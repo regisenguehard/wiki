3e26578c5797648f0f63cd34ce131dcba4fe6ab6
JF <jf@whatson-web.com>
2018-02-08 14:03:23 +0100

    exemples code style PHP

---
+++ Conventions/Exemples/Bien.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle;
+
+use Azerty/MyOtherClass;
+use Qwerty/MyClass;
+
+class MyClass {
+	private $myPrivateProperty = [];
+	public $publicProperty = null;
+	public $otherPublicProperty = null;
+	
+	public function myFunction($arg1, $arg2)
+	{
+		$myVar = 'azerty';
+		
+		if ($myVar == 'swaerty') {
+			$myOtherVar = 'test';
+		}
+		
+		if ($myVar == true) {
+			$myVar == 'Yo'.'Yo';
+		} else {
+			$myVar == 'Oy';
+		}
+		
+		$this->otherFunction();
+	}
+	
+	private myPrivateFunction() {
+	}
+}
---
+++ Conventions/Exemples/pas_bien.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle;
+
+use Qwerty/MyClass;
+use Azerty/MyOtherClass;
+use Azerty/UselessClass;
+
+class MyClass
+{
+
+	private $_myPrivateProperty = [];
+	$publicProperty = $otherPublicProperty = null;
+
+	function myFunction( $arg1,$arg2 ) {
+
+		$myVar = 'azerty';
+		
+		if ($myVar == 'swaerty') $myOtherVar = 'test';
+		
+		if($myVar == true)
+		{
+			$myVar == 'Yo' . 'Yo';
+		}else
+		{
+			$myVar == 'Oy';
+		}
+		
+		$myClass = new MyClass();
+		$myOtherClass = new MyOtherClass();
+
+		$this->otherFunction();
+
+	}
+	
+	private _myPrivateFunction() {
+	}
+
+}
